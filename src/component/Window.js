fad98be95050ab9a879679176ef163c269651ab5
Solo-steven <ssteven19@kimo.com>
2021-03-17 15:01:37 +0800

    於Window元件中補入await
    Window呼叫Container的函式時，可能會需要發出
    request，因此需要加上await。
    但不用也不會有bug，因為呼叫Container的函式
    都是於Window執行的最後一步，之後的re-render
    也是經由props改變導致的，只要確保Container的
    函式為同步即可。

--- src/component/Window.js
+++ src/component/Window.js
@@ -14,21 +14,21 @@ export default class Window extends React.Component{
         let window = this.window_ref.current;
         if( (window.scrollHeight - window.scrollTop === window.clientHeight) && (window.scrollTop!== 0) ){
             console.log(`Event : Reach Rottom (city :${this.props.city.name} , purpose : ${this.props.purpose} )`);
-            this.props.emitSignal_reachBottom(this.props.purpose, this.props.city.name);
+            await this.props.emitSignal_reachBottom(this.props.purpose, this.props.city.name);
         }
     }
     async componentDidMount(){
         console.log('window mount');
         this.window_ref.current.addEventListener('scroll', this.detectScrollToBottom);
         console.log(`Event : Filpe To New City (city :${this.props.city.name} , purpose : ${this.props.purpose} ).`);
-        this.props.emitSignal_flipCityPage(this.props.purpose , this.props.city.name); 
+        await this.props.emitSignal_flipCityPage(this.props.purpose , this.props.city.name); 
     }
     async componentDidUpdate(prevProps){
         console.log('window update');
         if(prevProps.city.name !== this.props.city.name || this.props.purpose !== prevProps.purpose){
             console.log(`Event : Filpe To New City :${this.props.city.name} `);
             this.window_ref.current.scrollTop = 0;
-            this.props.emitSignal_flipCityPage(this.props.purpose , this.props.city.name);    
+            await this.props.emitSignal_flipCityPage(this.props.purpose , this.props.city.name);    
         }
     }
     componentWillUnmount(){
